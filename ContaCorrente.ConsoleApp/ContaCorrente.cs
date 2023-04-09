caa94b2bb7af6a49e2ff123ba28d3a6d8fccc8b3
Davi Borges <davifelipe_300@hotmail.com>
2023-04-09 20:43:57 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class ContaCorrente
+    {
+        public int Id { get; set; }
+        public int Saldo { get; set; }
+        public int Numero { get; set; }
+        public int Limite { get; set; }
+        public bool Especial { get; set; }
+        public List<Movimentacao> Movimetacoes { get; set;}
+
+        public ContaCorrente()
+        {
+            
+        }
+        public void SacarValor()
+        {
+        }
+        public void DepositarValor()
+        {
+        }
+        public void VisualizarSaldo()
+        {
+        }
+        public void VisualizarExtrato()
+        {
+        }
+        public void TransferirValor()
+        {
+        }
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Movimentacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Movimentacao
+    {
+        public int Id { get; set; }
+        public int Valor { get; set; }
+        public string Informacao { get; set; }
+
+        public Movimentacao()
+        {
+            
+        }
+
+        void VisualizarMovimentacoes()
+        {
+
+        }
+    }
+}
